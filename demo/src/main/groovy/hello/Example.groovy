71f695d267d6cc9fdfd0ecae1a40d1bccfdabc02
Rani Koiguri <sandhya.rani.koiguri@accenture.com>
2017-08-28 16:32:59 +0530

    first commit

---
+++ demo/src/main/groovy/hello/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package hello
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
